df1ec092ef30d53cdaf89e0524c999b02ec4aafe
Isabelle <stettlerisabelle@gmail.com>
2026-08-05 15:35:33 -0400

    finally fix indetation?

--- src/components/Header/components/HeaderSearchButton.tsx
+++ src/components/Header/components/HeaderSearchButton.tsx
@@ -4,15 +4,15 @@ import { useState, useRef } from "react";
 
 import HeaderSearchForm from "./HeaderSearchForm";
 import {
-	Button,
-	Icon,
-	useCloseDropDown,
+  Button,
+  Icon,
+  useCloseDropDown,
 } from "@nypl/design-system-react-components";
 
 import { sendAnalyticsNavClickEvent } from "../../../utils";
 
 export interface HeaderSearchButtonProps {
-	isMobile?: boolean;
+  isMobile?: boolean;
 }
 
 /**
@@ -20,54 +20,54 @@ export interface HeaderSearchButtonProps {
  * keep focus trapped within the menu.
  */
 const HeaderSearchButton = chakra(
-	({ isMobile = false }: HeaderSearchButtonProps) => {
-		const [isOpen, setIsOpen] = useState<boolean>(false);
-		const styles = useStyleConfig("HeaderSearchButton", { isOpen });
-		const buttonText = isMobile ? null : isOpen ? "Close" : "Search";
-		const labelText = isOpen ? "Close Search" : "Open Search";
-		const ref = useRef<HTMLDivElement>(null);
+  ({ isMobile = false }: HeaderSearchButtonProps) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const styles = useStyleConfig("HeaderSearchButton", { isOpen });
+    const buttonText = isMobile ? null : isOpen ? "Close" : "Search";
+    const labelText = isOpen ? "Close Search" : "Open Search";
+    const ref = useRef<HTMLDivElement>(null);
 
-		useCloseDropDown(setIsOpen, ref);
+    useCloseDropDown(setIsOpen, ref);
 
-		return (
-			<Box ref={ref}>
-				<FocusLock isDisabled={!isOpen}>
-					<Button
-						aria-haspopup="true"
-						aria-label={labelText}
-						aria-expanded={isOpen ? true : null}
-						buttonType="text"
-						id="searchButton"
-						onClick={() => {
-							!isOpen && sendAnalyticsNavClickEvent({ clickText: "search" });
-							setIsOpen(!isOpen);
-						}}
-						__css={{
-							...styles,
-							color: isOpen
-								? "var(--nypl-colors-ui-white) !important"
-								: "ui.link.primary",
-							border: "none !important",
-							letterSpacing: 0,
-							padding: "0px",
-							paddingInlineEnd: "0px",
-						}}
-					>
-						<span>
-							{buttonText}
-							<Icon
-								align={isMobile ? "none" : "right"}
-								name={isOpen ? "close" : "actionSearch"}
-								size={isMobile ? "large" : "medium"}
-								title={labelText}
-							/>
-						</span>
-					</Button>
-					{isOpen && <HeaderSearchForm isMobile={isMobile} />}
-				</FocusLock>
-			</Box>
-		);
-	},
+    return (
+      <Box ref={ref}>
+        <FocusLock isDisabled={!isOpen}>
+          <Button
+            aria-haspopup="true"
+            aria-label={labelText}
+            aria-expanded={isOpen ? true : null}
+            buttonType="text"
+            id="searchButton"
+            onClick={() => {
+              !isOpen && sendAnalyticsNavClickEvent({ clickText: "search" });
+              setIsOpen(!isOpen);
+            }}
+            __css={{
+              ...styles,
+              color: isOpen
+                ? "var(--nypl-colors-ui-white) !important"
+                : "ui.link.primary",
+              border: "none !important",
+              letterSpacing: 0,
+              padding: "0px",
+              paddingInlineEnd: "0px",
+            }}
+          >
+            <span>
+              {buttonText}
+              <Icon
+                align={isMobile ? "none" : "right"}
+                name={isOpen ? "close" : "actionSearch"}
+                size={isMobile ? "large" : "medium"}
+                title={labelText}
+              />
+            </span>
+          </Button>
+          {isOpen && <HeaderSearchForm isMobile={isMobile} />}
+        </FocusLock>
+      </Box>
+    );
+  },
 );
 
 export default HeaderSearchButton;
